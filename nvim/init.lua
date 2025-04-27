40d0b6730a68f92049eb74f91b4a4931f503ae0a
Kay Grewe <Kay.Grewe@protonmail.com>
2025-04-27 17:07:39 +0200

    feat: Add window movement

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,4 +6,10 @@ vim.cmd[[colorscheme eldritch]]
 
 vim.o.number = true
 
+--Cursor movement between windows
+vim.keymap.set("n", "<M-j>", "<c-W>j", { desc = "Move cursor to the bottom window" })
+vim.keymap.set("n", "<M-k>", "<c-W>k", { desc = "Move cursor to the upper window"  })
+vim.keymap.set("n", "<M-hY", "<c-W>h", { desc = "Move cursor to the left window"   })
+vim.keymap.set("n", "<M-l>", "<c-W>l", { desc = "Move cursor to the right window"  })
+
 vim.opt.scrolloff = 8
